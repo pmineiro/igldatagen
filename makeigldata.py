27a66797f2e2e3a88d7ceba0aeaff0073ca7a96a
Paul Mineiro <paul.mineiro@gmail.com>
2024-05-03 10:42:55 -0700

    seems to work

--- makeigldata.py
+++ makeigldata.py
@@ -1,16 +1,24 @@
 #! /usr/bin/env python
 
 from datasets import load_dataset
+import json
 import os
 import torch
+from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
-def generate(*, messages, model, tokenizer):
+def simulator_prompt(mood):
+    return f"You are a user simulator.  A user has been presented a Question and an Answer. Simulate the user's next statement.  The user is {mood} with the Answer to the Question."
+
+def encapsulate_qa(question, answer):
+    return f"Question:\n{question}\n\nAnswer:\n{answer}\n"
+
+def generate(*, messages, model, tokenizer, num_return_sequences):
     # https://huggingface.co/docs/transformers/v4.39.3/en/llm_tutorial#wrong-prompt
     model_inputs = tokenizer.apply_chat_template(messages, add_generation_prompt=True, return_tensors="pt").to("cuda")
     input_length = model_inputs.shape[1]
-    generated_ids = model.generate(model_inputs, do_sample=True)
-    return tokenizer.batch_decode(generated_ids[:, input_length:], skip_special_tokens=True)[0]
+    generated_ids = model.generate(model_inputs, do_sample=True, max_new_tokens=100, num_return_sequences=num_return_sequences)
+    return tokenizer.batch_decode(generated_ids[:, input_length:], skip_special_tokens=True)
 
 # https://chat.lmsys.org/?leaderboard
 worse_model_id="Qwen/Qwen1.5-0.5B-Chat" # ELO not listed, so < 804
@@ -35,3 +43,32 @@ better_model = AutoModelForCausalLM.from_pretrained(better_model_id,
 better_tokenizer = AutoTokenizer.from_pretrained(better_model_id, add_special_tokens=False)
 
 dataset = load_dataset('lmsys/chatbot_arena_conversations', token=os.environ.get('token', None))
+
+with open('igldata.out', 'w') as file:
+    for datum in tqdm(dataset['train']):
+        question = datum['conversation_a'][0]['content']
+        messages = [ { 'role': 'user', 'content': question } ]
+        worse_answers = generate(messages=messages, model=worse_model, tokenizer=worse_tokenizer, num_return_sequences=4)
+        better_answers = generate(messages=messages, model=better_model, tokenizer=better_tokenizer, num_return_sequences=1)
+    
+        actions = []
+        for answer in worse_answers:
+            messages = [ { 'role': 'system', 'content': simulator_prompt("not satisfied") },
+                         { 'role': 'user', 'content': encapsulate_qa(question, worse_answers[0]) },
+                       ]
+            worse_user_response = generate(messages=messages, model=better_model, tokenizer=better_tokenizer, num_return_sequences=1)
+            actions.append({ 'action': answer,
+                             'feedback': worse_user_response,
+                             'reward': 0 })
+    
+        for answer in better_answers:
+            messages = [ { 'role': 'system', 'content': simulator_prompt("satisfied") },
+                         { 'role': 'user', 'content': encapsulate_qa(question, better_answers[0]) },
+                       ]
+            better_user_response = generate(messages=messages, model=better_model, tokenizer=better_tokenizer, num_return_sequences=1)
+            actions.append({ 'action': answer,
+                             'feedback': better_user_response,
+                             'reward': 1 })
+    
+        igldatum = { 'question': question, 'actions': actions }
+        print(json.dumps(igldatum), file=file, flush=True)
